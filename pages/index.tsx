5ff344ec64741604d261bd0abe9af9266a9a72f7
Nimesh404 <57608111+Nimesh-bot@users.noreply.github.com>
2024-04-11 17:56:24 +0545

    chore: metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import CarouselSection from "@/components/home/CarouselSection";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Create Himalayan Adventure',
+  description: ''
+}
 
 export default function Home() {
   return (
-    <section
-      className='flex flex-col gap-y-16'
-    >
-      <CarouselSection />
-      <WhyChooseUs />
-    </section>
+      <section
+        className='flex flex-col gap-y-16'
+      >
+        <CarouselSection />
+        <WhyChooseUs />
+      </section>
   );
 }
