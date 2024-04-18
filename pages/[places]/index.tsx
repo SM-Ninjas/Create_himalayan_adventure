13c8fd95f63ddf6b74795c498a1ab0dcb48bb138
sushant babu prasai <sushantprasaii7@gmail.com>
2024-04-18 18:59:02 +0545

    Feat: added dummy carousel and  overview

--- pages/[places]/index.tsx
+++ pages/[places]/index.tsx
@@ -1,45 +1,58 @@
-import { sliderData } from "@/components/home/CarouselSection";
+import { sliderData, subImages } from "@/components/home/CarouselSection";
 import Carousel from "@/components/ui/carousel";
 import { useRouter } from "next/router";
 import React from "react";
 import { FaClock } from "react-icons/fa";
 import { MdGroups } from "react-icons/md";
 import { Data } from "@/components/home/CarouselSection";
+import OverView from "@/components/ui/overView";
 
 function PlacesDetails() {
   const router = useRouter();
-  const { places } = router.query;
+  const index = router?.query.index;
 
-  const filtered: Data[] = sliderData.filter(
-    (slider) => slider.location === places
+  const place: Data | undefined = sliderData.find(
+    (item) => item.id.toString() === index
   );
- 
+
+  const subImg: subImages = place?.subImages || [];
 
   return (
     <div className="mt-[100px] flex w-full justify-center">
-      <div className="w-[80%] border">
-        <div className=" ">
-          {filtered.map((place) => (
-            <div key={place.id}>
-              <div className="">
-                <h1 className="text-[24px] font-bold">{place.title}</h1>
-                <div className="mb-[30px] flex gap-[30px]">
-                  <p className="flex items-center gap-2 text-[#1F1F1F]">
-                    <FaClock size={20} />
-                    {place.days}
-                  </p>
-                  <p className="flex items-center gap-2 text-[#1F1F1F]">
-                    <MdGroups size={30} />
-                    {place.people}
-                  </p>
-                </div>
+      <div className="w-[80%]">
+        <div className="">
+          <div key={place?.id}>
+            <div className="">
+              <h1 className="text-[24px] font-bold">{place?.title}</h1>
+              <div className="mb-[30px] flex gap-[30px]">
+                <p className="flex items-center gap-2 text-[#1F1F1F]">
+                  <FaClock size={20} /> {place?.days}{" "}
+                </p>
+
+                <p className="flex items-center gap-2 text-[#1F1F1F]">
+                  <MdGroups size={30} />
+                  {place?.people}
+                </p>
               </div>
             </div>
-          ))}
+          </div>
         </div>
-        <div className="">
-          {/* here lies the carousel */}
-          <Carousel filteredData={filtered} />
+
+        <div className="flex gap-[45px]">
+          <div className="w-[60%]">
+            <div key={place?.id}>
+              <img src={place?.img} className="rounded-[14px]" alt="" />
+            </div>
+          </div>
+          <div className="w-[40%]">
+            <Carousel images={subImg} />
+          </div>
+        </div>
+        <div className="my-[30px]">
+          <h1 className="text-[20px] font-semibold">Overview</h1>
+          <div className="w-[60%]">
+            <OverView placeData={place} />
+          </div>
         </div>
       </div>
     </div>
@@ -47,3 +60,5 @@ function PlacesDetails() {
 }
 
 export default PlacesDetails;
+
+//
