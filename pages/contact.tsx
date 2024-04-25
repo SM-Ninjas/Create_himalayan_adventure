20bd4c5eaf2120f992430447f5e1ded8602da634
sushant babu prasai <sushantprasaii7@gmail.com>
2024-04-25 23:04:42 +0545

    Feat: blogs and contact form

---
+++ pages/contact.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import mainImg from "../public//images/upHill.png"
+import Image from 'next/image'
+import Form from '@/components/form'
+import image from "../public/images/googleMap.png"
+
+function Contact() {
+    return (
+        <div className=''>
+            <div className='relative'>
+                <Image src={mainImg} className='w-full h-[430px]' alt="mainImage" />
+                <h1 className='absolute left-[24rem] text-3xl bottom-10 text-white '>Contact Us</h1>
+            </div>
+            <div className=' container  flex mt-8'>
+                <div className='w-1/2'>
+                    <Form />
+                </div>
+                <div className='w-1/2 '>
+                    <Image src={image} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
