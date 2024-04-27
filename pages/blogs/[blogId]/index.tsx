a535cb00648ed340518e1b58b6b8a619b9a7d409
sushant babu prasai <sushantprasaii7@gmail.com>
2024-04-27 13:11:22 +0545

    Feat: added calender

--- pages/blogs/[blogId]/index.tsx
+++ pages/blogs/[blogId]/index.tsx
@@ -6,15 +6,17 @@ function Blogs() {
     const router = useRouter()
     const blogId = router.query.blogId
     const currentBlogData = blogPosts?.find((blog) => (blog.id.toString() === blogId));
+    // const featuredImage = currentBlogData?.featured_image || ''; // Provide a default value for the image source
     return (
         <div className='container mt-[6rem]'>
-            <h1 className='text-2xl'>
+            <h1 className='text-2xl font-semibold text-center'>
                 {currentBlogData?.title}
             </h1>
-            <p className='text-gray-600 text-sm'>{currentBlogData?.content}</p>
+            <p className='text-center mt-2 mb-4 text-[#0075FF] '>{currentBlogData?.publish_date}</p>
+            <img src={currentBlogData?.featured_image} alt='blog image' className='w-full h-[600px]' />
+            <p className='text-gray-600 text-sm mt-6'>{currentBlogData?.content}</p>
         </div>
     )
 }
-
 export default Blogs
 
