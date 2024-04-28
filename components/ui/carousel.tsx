5ff5d0ee28e9547d27c2a54401978ffb57dd6692
sushant babu prasai <sushantprasaii7@gmail.com>
2024-04-28 23:23:47 +0545

    Carousel changes

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { DataTypes as Place, subImages } from "../home/CarouselSection";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
@@ -14,14 +13,14 @@ interface CarouselProps {
 function Carousel({ images }: CarouselProps) {
 
   return (
-    <div className="w-full">
+    <div className="w-full border p-1">
       <div className="">
         <div className="flex gap-[30px]">
           <Swiper
             className=" w-full mySwiper"
             slidesPerView={2}
             slidesPerGroup={2}
-            spaceBetween={20}
+            spaceBetween={30}
             loop={true}
             autoplay={{
               delay: 2000,
@@ -42,8 +41,6 @@ function Carousel({ images }: CarouselProps) {
                   width={500}
                   height={200}
                   className="rounded-lg"
-                // className="h-[250px] w-[900px] cursor-pointer rounded-[14px] object-cover"
-                // alt=""
                 />
               ))}
             </SwiperSlide>
