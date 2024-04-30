054cd6036554efb2e612288f6f9c946b8a2a1260
sushant babu prasai <sushantprasaii7@gmail.com>
2024-04-30 13:58:16 +0545

    Fix: image draggable fix

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -1,50 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 import { DataTypes as Place, subImages } from "../home/CarouselSection";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
-
-
 
 interface CarouselProps {
   images: subImages;
 }
 
 function Carousel({ images }: CarouselProps) {
-
   return (
     <div className="w-full p-1">
-      <div className="">
-        <div className="flex gap-[30px]">
-          <Swiper
-            className="w-full mySwiper overflow-auto"
-            slidesPerView={2}
-            slidesPerGroup={2}
-            spaceBetween={30}
-            navigation={true}
-            // loop={true}
-            autoplay={{
-              delay: 2000,
-              disableOnInteraction: false,
-            }}
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-          >
-            <SwiperSlide className="flex gap-4">
-              {images.map((image, index) => (
+      <div className="border gap-[30px]">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          loop={false}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          navigation
+          pagination={{ clickable: true }}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index} className="">
+              <div className="flex justify-center">
                 <Image
                   src={image}
                   alt={image}
                   width={500}
                   height={200}
-                  className="rounded-lg"
+                  className="rounded-lg w-full h-[280px]"
                 />
-              ))}
+              </div>
             </SwiperSlide>
-          </Swiper>
-        </div>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
