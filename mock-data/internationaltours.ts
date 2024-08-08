015221089c13f62b2b2d66b82b2e5924f59f9440
vivekpandey2717340 <vivek2717340@gmail.com>
2024-08-08 16:25:17 +0545

    Bhutan and tibet Added

--- mock-data/internationaltours.ts
+++ mock-data/internationaltours.ts
@@ -9,6 +9,134 @@ export const internationalRegions: TrekAndTours[] = [
     {
         name: "Bhutan",
         options: [
+        ],
+        route: routes.treks,
+      },
+      {
+        name: "Trekking and Hiking in Bhutan",
+        options: [
+          {
+            name: "Snowman Trek - 30 days",
+            slug: "snowman_trek_30_days",
+          },
+          {
+            name: "Bhutan Tour With Bumdra High Camp Trek",
+            slug: "Bhutan_Tour_with_Bumdra_High_Camp_Trek",
+          },
+          {
+            name: "Druk Path Trek - 8 days",
+            slug: "DrukPathTrek",
+          },
+          {
+            name: "Dagala Thousand Lakes Trek - 8 days",
+            slug: "Dagala_Thousand_Lakes_Trek",
+          },
+          {
+            name: "Jomolhari Trek - 11 days",
+            slug: "Jomolhari_Trek",
+          },
+          {
+            name: "Soi Yaksa Trek",
+            slug: "Soi_Yaksa_Trek",
+          },
+        ],
+        route: routes.treks,
+      },
+      {
+        name: "Culture and City Tour in Bhutan",
+        options: [
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+        ],
+        route: routes.treks,
+      },
+      {
+        name: "",
+        options: [
+        ],
+        route: routes.treks,
+      },
+      {
+        name: "Day Hikes And Cultural Tour in Bhutan",
+        options: [
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+        ],
+        route: routes.treks,
+      },
+      {
+        name: "Festival Tour in Bhutan",
+        options: [
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+        ],
+        route: routes.treks,
+      }, {
+        name: "Tibet",
+        options: [
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+          {
+            name: "",
+            slug: "",
+          },
+        ],
+        route: routes.treks,
+      }, {
+        name: "Culture And City Tours in Tibet",
+        options: [
           {
             name: "",
             slug: "",
