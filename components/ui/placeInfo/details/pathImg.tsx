fc39a5b2ac0921b9f59fcd1ada35ee4e454bd05c
vivekpandey2717340 <vivek2717340@gmail.com>
2024-08-09 16:32:19 +0545

    google map added

--- components/ui/placeInfo/details/pathImg.tsx
+++ components/ui/placeInfo/details/pathImg.tsx
@@ -1,12 +1,41 @@
 import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
-interface pathImageTypes {
+interface PathImageTypes {
   pathImg?: string | undefined;
+  mapCenter: google.maps.LatLngLiteral; // Add center for map
+  zoom: number; // Add zoom level for the map
 }
-function PathImg(pathImg: pathImageTypes) {
+
+function PathImg({ pathImg, mapCenter, zoom }: PathImageTypes) {
+  // Define the container style for the map
+  const containerStyle = {
+    width: '800px',
+    height: '400px'
+  };
+
   return (
-    <div className="mt-4">
-      <img src={pathImg?.pathImg} height={400} width={800} alt="myimg" />
+    <div>
+      {/* <div className="mt-4">
+        <img
+          src={pathImg}
+          height={400}
+          width={800}
+          alt="Path Image"
+        />
+      </div> */}
+
+      <div className="mt-4">
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ''}>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={mapCenter}
+            zoom={zoom}
+          >
+            {/* You can add markers or other components here */}
+          </GoogleMap>
+        </LoadScript>
+      </div>
     </div>
   );
 }
